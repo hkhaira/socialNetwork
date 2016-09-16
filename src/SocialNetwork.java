9b51078bb98dfb7cac7c31e81e23e250e9ba1d7d
Harpreet Khaira <K@Khaira.local>
2016-09-16 03:54:05 -0700

    Only one user can be online + added .gitignore

--- src/SocialNetwork.java
+++ src/SocialNetwork.java
@@ -5,9 +5,9 @@ import org.mockito.internal.stubbing.answers.ReturnsElementsOf;
 import java.util.Collection;
 
 public class SocialNetwork implements ISocialNetwork {
-	
+
 	private Account currentUser = null;
-	
+
 	private Collection<Account> accounts = new HashSet<Account>();
 
 	// join SN with a new user name
@@ -44,34 +44,12 @@ public class SocialNetwork implements ISocialNetwork {
 		return members;
 	}
 
-	// from my account, send a friend request to user with userName from my
-	// account
-	public void sendFriendshipToOld(String userName, Account me) {
-		if (listMembers().contains(userName)) {
-			Account accountForUserName = findAccountForUserName(userName);
-			accountForUserName.requestFriendship(me);
-		}
-	}
-
-	// from my account, accept a pending friend request from another user with
-	// userName
-	public void acceptFriendshipFromOld(String userName, Account me) {
-		Account accountForUserName = findAccountForUserName(userName);
-		accountForUserName.friendshipAccepted(me);
-	}
-
 	// from my account, accept all the pending friend requests at once
 	public void acceptAllFriendships() {
 		Object[] incomingRequests = currentUser.getIncomingRequests().toArray();
 		for (Object each : incomingRequests) {
 			acceptFriendshipFrom(each.toString());
 		}
-	}
-
-	// From my account, reject friendship request from another member
-	public void rejectFriendshipFromOld(String userName, Account me) {
-		Account accountForUserName = findAccountForUserName(userName);
-		accountForUserName.friendshipRejected(me);
 	}
 
 	// From my account, reject all the pending friendship requests
@@ -82,39 +60,6 @@ public class SocialNetwork implements ISocialNetwork {
 		}
 	}
 
-	// From my account, auto accept all the future friendship request
-	public void autoAcceptFriendshipsToOld(Account me) {
-		me.autoAcceptFriendships();
-	}
-
-	// From my account, send a friendship cancellation to another member
-	public void sendFriendshipCancellationToOld(String userName, Account me) {
-		Account accountForUsername = findAccountForUserName(userName);
-		accountForUsername.cancelFriendship(me);
-	}
-
-	// From my account, leave social networks
-	public void leaveOld(Account me) {
-		Object[] friends = me.getFriends().toArray();
-		for (Object each : friends) {
-			sendFriendshipCancellationTo(each.toString());
-		}
-
-		Object[] incomingRequests = me.getIncomingRequests().toArray();
-		for (Object each : incomingRequests) {
-			rejectFriendshipFrom(each.toString());
-		}
-
-		Object[] outgoingRequests = me.getOutgoingRequests().toArray();
-		Account leavingUser = me;
-		for (Object each : outgoingRequests) {
-			//TODO: rename variables
-			login(findAccountForUserName(each.toString()));
-			rejectFriendshipFrom(leavingUser.getUserName());
-		}
-		accounts.remove(me);
-	}
-
 	public Account login(Account me) {
 		currentUser = me;
 		return currentUser;
@@ -122,7 +67,7 @@ public class SocialNetwork implements ISocialNetwork {
 
 	public void logout() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public boolean hasMember(String userName) {
@@ -130,44 +75,56 @@ public class SocialNetwork implements ISocialNetwork {
 		return false;
 	}
 
+	// from my account, send a friend request to user with userName from my
+	// account
 	public void sendFriendshipTo(String userName) {
-		sendFriendshipToOld(userName, currentUser);
-		
+		if (listMembers().contains(userName)) {
+			Account accountForUserName = findAccountForUserName(userName);
+			accountForUserName.requestFriendship(currentUser);
+		}
+
 	}
 
 	public void block(String userName) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public void unblock(String userName) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
+	// From my account, send a friendship cancellation to another member
 	public void sendFriendshipCancellationTo(String userName) {
-		sendFriendshipCancellationToOld(userName, currentUser);
-		
+		Account accountForUsername = findAccountForUserName(userName);
+		accountForUsername.cancelFriendship(currentUser);
+
 	}
 
+	// from my account, accept a pending friend request from another user with
+	// userName
 	public void acceptFriendshipFrom(String userName) {
-		acceptFriendshipFromOld(userName, currentUser);
-		
+		Account accountForUserName = findAccountForUserName(userName);
+		accountForUserName.friendshipAccepted(currentUser);
+
 	}
 
+	// From my account, reject friendship request from another member
 	public void rejectFriendshipFrom(String userName) {
-		rejectFriendshipFromOld(userName, currentUser);
-		
+		Account accountForUserName = findAccountForUserName(userName);
+		accountForUserName.friendshipRejected(currentUser);
+
 	}
 
+	// From my account, auto accept all the future friendship request
 	public void autoAcceptFriendships() {
-		autoAcceptFriendshipsToOld(currentUser);
-		
+		currentUser.autoAcceptFriendships();
 	}
 
 	public void cancelAutoAcceptFriendships() {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	public Collection<String> recommendFriends() {
@@ -175,9 +132,27 @@ public class SocialNetwork implements ISocialNetwork {
 		return null;
 	}
 
+	// From my account, leave social networks
 	public void leave() {
-		leaveOld(currentUser);
-		
+		Object[] friends = currentUser.getFriends().toArray();
+		for (Object each : friends) {
+			sendFriendshipCancellationTo(each.toString());
+		}
+
+		Object[] incomingRequests = currentUser.getIncomingRequests().toArray();
+		for (Object each : incomingRequests) {
+			rejectFriendshipFrom(each.toString());
+		}
+
+		Object[] outgoingRequests = currentUser.getOutgoingRequests().toArray();
+		Account leavingUser = currentUser;
+		for (Object each : outgoingRequests) {
+			// TODO: rename variables
+			login(findAccountForUserName(each.toString()));
+			rejectFriendshipFrom(leavingUser.getUserName());
+		}
+		accounts.remove(currentUser);
+
 	}
 
 }
